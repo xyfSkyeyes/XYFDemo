6b6ac15767a506dcc0336030cb0b04f2a84ed5bf
xmg <xmg@qq.com>
2016-12-20 21:20:11 +0800

    1111111111111

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by xyf on 2016/12/20.
+//  Copyright © 2016年 xmg20. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by xyf on 2016/12/20.
+//  Copyright © 2016年 xmg20. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
